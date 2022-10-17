3fdb74ca62c33b84c64662a965533dae72a5cc3e
Nilesh Mandal <nilemandal@yahoo.com>
2022-10-17 08:27:07 +0530

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application, Request, Response } from "express";
+const app:Application = express();
+
+app.get('/', (req:Request,res:Response)=>{
+    res.send('Hello')
+})
+
+app.listen(3000,()=>{console.log('Server Running')});
