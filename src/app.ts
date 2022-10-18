1000fbb34dcbe954c03c0a6db31184482f2cc0ec
Nilesh Mandal <nilemandal@yahoo.com>
2022-10-19 02:55:56 +0530

    helmet

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express, { Application, Request, Response } from 'express';
+import helmet from 'helmet';
+
 const app: Application = express();
+
+app.use(
+	helmet({
+		contentSecurityPolicy: false,
+	})
+);
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello');
