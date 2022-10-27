75eef283f0749ced453f89357ad8280d24a994bb
Nilesh Mandal <nilemandal@yahoo.com>
2022-10-27 06:40:14 +0530

    scss setup

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,7 @@ app.engine(
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'public')));
 
 app.use(
 	helmet({
